6ec337298e0c7b1df12c54ade3c06a3392235750
t-ha <tjunkha@gmail.com>
2016-04-13 18:52:06 -0700

    names, disappear

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -15,18 +15,27 @@ var people2 = [
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
   // Create a varaible myDiv by selecting the element with id my-div (this is already created for you).
+var myDiv = d3.select('#my-div');
 
 
   // Create a variable `paragraphs` by selecting all  `p` elements in  `myDiv`, and binding you data  `people` to it.
   // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
-
+var paragraphs = myDiv.selectAll('p').data(people, function(p) {return p.name});
 
 
   // Add a paragraph to the div for each person in your dataset following instructions in the index.html file
-
+paragraphs.enter()
+          .append('p')
+          .text(function(n) {return n.name})
+          .style('color', function(n) {return n.favColor});
 
   // Use `setTimeout` function to wait 3 seconds, then do the following
-
+setTimeout(function() {
+    var paragraphs = myDiv.selectAll('p')
+                          .data(people2, function(p) {return p.name});
+    paragraphs.exit()
+              .remove();
+}, 3000);
     // Re-define your variable `paragraphs` by selecting all `p` elements in `myDiv`, and binding the data `people2` to it.
     // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
 
